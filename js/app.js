06f57bb159969707688c8519e7db4af587706de7
Christian Lee <cjlee300@gmail.com>
2023-09-10 19:51:46 -0400

    Add task completion functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 const db = {
-    data: [],
+    data: {},
     readAll: function() {
         console.log('Returning all data: ', this.data);
-        return data;
+        return this.data;
     },
     newTaskInput: function(name) {
         const newTask = this._createTask(name);
-        this.data.push(newTask);
+        this.data[newTask.id] = newTask.name;
         return newTask;
+    },
+    completeTask: function(taskId) {
+        delete this.data[taskId];
     },
     _createTask: function(name) {
         return {
@@ -23,9 +26,13 @@ function createTaskListItem(task) {
     const taskItem = document.createElement('li');
     const checkBox = document.createElement('input');
     const checkBoxLabel = document.createElement('label');
+
     checkBox.type = 'checkbox';
+    checkBox.id = task.id;
+    checkBox.onchange = handleCheckBoxChange;
     checkBoxLabel.textContent = ' ' + task.name;
-    checkBoxLabel.htmlFor = task;
+    checkBoxLabel.htmlFor = task.id;
+
     taskItem.appendChild(checkBox);
     taskItem.appendChild(checkBoxLabel)
     taskList.appendChild(taskItem);
@@ -39,6 +46,14 @@ function handleSubmit(event) {
 
     createTaskListItem(newTask);
     return false;
+}
+
+function handleCheckBoxChange(event) {
+    event.preventDefault();
+    const taskItem = event.target;
+    db.completeTask(taskItem.id);
+    taskItem.parentNode.remove();
+    db.readAll();
 }
 
 form.addEventListener('submit', handleSubmit);
